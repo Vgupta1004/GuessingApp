ab125296779119a04c43ffff664c5122f7ef8631
gupta <vgup3012@7-11.com>
2026-02-23 11:20:01 +0530

    Coded UC1 to display the Rules of the Guessing App demonstrating Encapsulation and Constructor initialization

---
+++ GuessingApp.java
@@ -0,0 +1,7 @@
+public class GuessingApp{
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Guessing Game!");
+        GameConfig config = new GameConfig();
+        config.showRules();
+    }
+}
